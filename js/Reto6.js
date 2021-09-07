3a8563af520da80fcd91035e1674ee6aa6ba853a
IvanMelo1 <ivanmauriciomm15@gmail.com>
2021-09-07 18:27:32 -0500

    2do avance retos

--- js/Reto6.js
+++ js/Reto6.js
@@ -1,19 +1,30 @@
 var muestra = " ";
+var sumaar = 0;
 
 function calcularmedia(){
-    muestra = document.getElementById('muestra-no').value;    
+    suma = 0;
+    sumaar = 0;
+    muestra = document.getElementById('muestra-no').value;
+    for(let i = 0; i<muestra.length; i++){
+        muestra = muestra.replace("," , "");
+    }
+    
     let array = muestra.split("");
 
+    console.log(array);
 
-    for(let i = 0; i<array.length; i = i + 2){
-        var suma = 0;
-        var numero = Number(array[i]);
-        suma = suma + numero;
-        console.log(array[i]);
+    for(let i = 0; i<array.length; i++){
+        suma += Number(array[i]);
+    }    
+    
+    var media = (suma/array.length).toFixed(2);
 
+    for(let i = 0; i<array.length; i++){
+        sumaar += (Number(array[i])-media)**2;
     }
 
-    console.log(suma);
-    
+   var sm = 
+    document.getElementById("salida6").innerHTML = "la media de su muestra de números es: " + media + " y su desviación estandar es "
+    console.log(sumaar)
 }
 
